0a49159d2d3de96c02917597e1a8c9bad2f60888
Md. Nafes Imtiaz <imtiaz.nafes@gmail.com>
2022-06-30 18:16:12 +0600

    code refactoring

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const helmet = require('helmet');
 const bodyParser = require("body-parser");
 const AdminRouter = require("./routes/adminRoute");
-const TeacherRouter = require("./api/teachers/teacher.router");
-const StudentRouter = require("./api/students/student.router");
 
 const app = express();
 //HTTP security header
@@ -16,8 +14,6 @@ app.get('/',(req,res)=>{
     res.send('Strativ Virtual Classroom API')
 })
 app.use('/api/admin', AdminRouter);
-app.use('/api/teacher', TeacherRouter);
-app.use('/api/student', StudentRouter);
 
 app.listen(process.env.APP_PORT, ()=>{
     console.log("Server Running on http://localhost:",process.env.APP_PORT)
